7e58f56ecea3e18cbf142600f72ed7ca8cc2e1be
felix-rojas <a01201946@tec.mx>
2023-05-19 10:14:44 -0600

    add flock model

--- Labs/Lab1/Assets/Scripts/PlayerController.cs
+++ Labs/Lab1/Assets/Scripts/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
     /// Global vars for camera controls for <c>PlayerController</c>
     public Camera mainCamera;
     public Camera hoodCamera;
-    /// InputKey for PLayer to switch camera
+    /// InputKey for Player to switch camera
     public KeyCode switchKey; 
     /// </summary>
 
@@ -43,15 +43,16 @@ public class PlayerController : MonoBehaviour
     {
         /// Get user input to turn
         horizontalInput = Input.GetAxis("Horizontal" + inputId);
+        
         /// Get user input to move
         forwardInput = Input.GetAxis("Vertical" + inputId);
 
-        /// translate to fwd and bwd movement
+        /// Translate to fwd and bwd movement
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        /// translate vector using rotation on the whole object
+        /// Translate vector using rotation on the whole object
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
-        /// Camera Switch when <param>switchkey</param> is pressed
+        /// Camera Switch <param name="switchKey">switchKey is the defined camera</param> is pressed
         if(Input.GetKeyDown(switchKey))
         {
             mainCamera.enabled = !mainCamera.enabled;
